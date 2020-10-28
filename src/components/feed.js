da1f4c58fb99ebebfc9a6eddc7f01c43cd1cd33a
Oscar Islas <oislasreyes@gmail.com>
2020-10-27 20:41:47 -0600

    Creando nuestro componente Tweet y Feed

---
+++ src/components/feed.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Tweet from './tweet/tweet';
+
+//Función regular
+//JSX (Javascript + XML)
+//XML - escribir etiquetas personalizas
+// - Distingue entre mayusculas y minusculas
+// - Sintaxis estricta
+export default function Feed(){
+ return (
+    <div>
+        <Tweet />
+    </div>
+ )
+}
+
+//Función anonima
+// const Feed = function Feed(){
+//  return 
+// }
+
+//Functión de flecha
+// const Feed = () => {
+//  return 
+// }
---
+++ src/components/tweet/tweet.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles.css';
+
+const Tweet = () => {
+    return (
+        <div className="tweet-container">
+            <div className="row">
+                <div className="c1">
+                    <img src="https://hipertextual.com/files/2012/06/twitter-bird-white-on-blue.jpg" alt="profile" />
+                </div>
+                <div className="c2">
+                    <h6>Twitter Safety</h6>
+                    <h6>@TwitterSafety</h6>
+                </div>
+                <div className="c3"></div>
+            </div>
+            <div className="row">
+                <p>                
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut risus et enim sodales congue sed ac ante. Maecenas tincidunt tortor vel lorem semper vestibulum. Phasellus placerat arcu ac dui ultricies, sed tempor dolor rhoncus. Cras id leo sem. Etiam bibendum fringilla justo vel feugiat. Vestibulum imperdiet tortor vitae ornare auctor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris auctor urna sit amet sagittis eleifend. In quis lorem pretium, commodo dui quis, tristique augue. Sed eget vestibulum ante, eget viverra turpis. Sed a velit pulvinar, egestas justo sit amet, elementum tortor. Sed tempor lectus magna, vitae vulputate ante auctor in. Integer ac orci ultrices, convallis risus non, lacinia leo. Nam et ex eleifend, porttitor lorem nec, scelerisque ligula.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Tweet;
